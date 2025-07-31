f7ae16c2ff2254a67a2176a536d0d3115f87a57b
wangxiaokangty <1002075358@qq.com>
2025-07-31 15:20:43 +0800

    add format

--- makefile
+++ makefile
@@ -1,5 +1,12 @@
+.PHONY: lint format
+
+all: lint format
+
 ruff:
-	uv run ruff check . --fix
+	uvx ruff check . --fix
+
+format:
+	uvx ruff format .
 
 test:
 	uv run -m pytest
--- tests/test_llm_client.py
+++ tests/test_llm_client.py
@@ -16,6 +16,7 @@ def test_client_initialize_from_dotenv():
     client = LLMClient()
     assert client is not None
 
+
 def test_struct_output():
     client = LLMClient()
     result = client.struct_generate("请给我随机生成一个人物", Character)
